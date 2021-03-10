6452234143a3a5b1ccda9fa82a78364a51c7b745
Yuto23 <yuto_itazu@towa-gifu.co.jp>
2021-03-10 18:39:12 +0900

    updated

--- app.js
+++ app.js
@@ -1,27 +1,28 @@
 const express = require('express');
 const mysql = require('mysql');
 const app = express();
+// const canvasDatagrid = require('canvas-datagrid')
 // データベース接続情報
-var conn = mysql.createConnection({
+const connection = mysql.createConnection({
     host:"localhost",
     port:3306,
     user:"root",
     password:null,
     database:'PaxEvan'
 });
-conn.query("create database PaxEvan;",(err)=>{
-    console.log("1:",err);
-    conn.query("create table PaxEvan.dt(id int,name varchar(12));",(err)=>{
-        console.log("2:",err);
-        conn.query("insert PaxEvan.dt value(1,'aaaa');",(err)=>{
-            console.log("3:",err);
-            conn.query("select * from PaxEvan.dt;",(err,re)=>{
-                console.log("4:",err);
-                conn.end()
-            })
-        })
-    })
-});
+//conn.query("create database PaxEvan;",(err)=>{
+    //console.log("1:",err);
+    //conn.query("create table PaxEvan.dt(id int,name varchar(12));",(err)=>{
+        //console.log("2:",err);
+        //conn.query("insert PaxEvan.dt value(1,'aaaa');",(err)=>{
+            //console.log("3:",err);
+            //conn.query("select * from PaxEvan.dt;",(err,re)=>{
+                //console.log("4:",err);
+                //conn.end()
+            //})
+        //})
+    //})
+//});
 
 // テーブルdtのデータを取得してindex.ejsで表示
 app.get('/', (req, res) => {
