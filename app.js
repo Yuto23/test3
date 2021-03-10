a2579efdefb2de0b4bd3728648958e10efc218ba
Yuto23 <yuto_itazu@towa-gifu.co.jp>
2021-03-10 19:46:35 +0900

    疲れた。w

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mysql = require('mysql');
 const app = express();
+const path = require('path')
+
 // const canvasDatagrid = require('canvas-datagrid')
 // データベース接続情報
 const connection = mysql.createConnection({
@@ -25,13 +27,30 @@ const connection = mysql.createConnection({
 //});
 
 // テーブルdtのデータを取得してindex.ejsで表示
-app.get('/', (req, res) => {
-  connection.query(
-    'SELECT * FROM dt',
-    (error, results) => {
-      res.render('index.ejs',{dt:results});
-    }
-  );
+//app.get('/', (req, res) => {
+  //connection.query(
+   // 'SELECT * FROM dt',
+//     (error, results) => {
+//       res.render('index.ejs',{dt:results});
+//     }
+//   );
+// });
+
+//index.htmlに変えた
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"))
 });
+
+  
+
+  //app.get('/data', (req, res) => {
+    //connection.query(
+      //  'SELECT * FROM dt',
+        //(error, results) => {
+          //res.json(results);
+        //}
+//    );    
+  //});
+
 
 app.listen(3000);
